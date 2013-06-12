bbda971225671c653097dfa424a66f912faba572
Ewan Mellor <ewan@tipbit.com>
2013-06-12 10:27:37 -0700

    Added.

---
+++ TidbitsTests/TidbitsTests.h
@@ -0,0 +1,13 @@
+//
+//  TidbitsTests.h
+//  TidbitsTests
+//
+//  Created by Ewan Mellor on 6/12/13.
+//  Copyright (c) 2013 Tipbit, Inc. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TidbitsTests : SenTestCase
+
+@end
