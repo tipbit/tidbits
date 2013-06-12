cb1f45306ddd1bd046735a13ab3d699540109cb9
Ewan Mellor <ewan@tipbit.com>
2013-06-12 12:06:42 -0700

    Added NSString+Misc.

---
+++ Tidbits/NSString+Misc.h
@@ -0,0 +1,29 @@
+//
+//  NSString+Misc.h
+//  Tipbit
+//
+//  Created by Ewan Mellor on 5/23/13.
+//  Copyright (c) 2013 Tipbit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Misc)
+
+-(bool) contains:(NSString*)substring;
+
+-(bool) endsWithCaseInsensitive:(NSString *)comparand;
+
+-(bool) endsWithChar:(unichar)c;
+
+-(bool) isEqualToStringCaseInsensitive:(NSString*)comparand;
+
+-(bool) startsWith:(NSString *)comparand;
+
+-(bool) startsWithCaseInsensitive:(NSString*)comparand;
+
+-(bool) startsWithChar:(unichar)c;
+
+-(NSString*)trim;
+
+@end
---
+++ Tidbits/NSString+Misc.m
@@ -0,0 +1,62 @@
+//
+//  NSString+Misc.m
+//  Tipbit
+//
+//  Created by Ewan Mellor on 5/23/13.
+//  Copyright (c) 2013 Tipbit. All rights reserved.
+//
+
+#import "NSString+Misc.h"
+
+@implementation NSString (Misc)
+
+-(bool)contains:(NSString *)substring {
+    return [self rangeOfString:substring].location != NSNotFound;
+}
+
+
+-(bool)endsWithCaseInsensitive:(NSString *)comparand {
+    if (comparand.length > self.length)
+        return false;
+    NSString* bit = [self substringFromIndex:self.length - comparand.length];
+    return [bit caseInsensitiveCompare:comparand] == NSOrderedSame;
+}
+
+
+-(bool)endsWithChar:(unichar)c {
+    return self.length == 0 ? false : c == [self characterAtIndex:self.length - 1];
+}
+
+
+-(bool)isEqualToStringCaseInsensitive:(NSString *)comparand {
+    return [self caseInsensitiveCompare:comparand] == NSOrderedSame;
+}
+
+
+-(bool)startsWith:(NSString *)comparand {
+    if (comparand.length > self.length)
+        return false;
+    NSString* bit = [self substringToIndex:comparand.length];
+    return [bit isEqualToString:comparand];
+}
+
+
+-(bool)startsWithCaseInsensitive:(NSString *)comparand {
+    if (comparand.length > self.length)
+        return false;
+    NSString* bit = [self substringToIndex:comparand.length];
+    return [bit caseInsensitiveCompare:comparand] == NSOrderedSame;
+}
+
+
+-(bool)startsWithChar:(unichar)c {
+    return self.length == 0 ? false : c == [self characterAtIndex:0];
+}
+
+
+-(NSString*)trim {
+    return [self stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+}
+
+
+@end
