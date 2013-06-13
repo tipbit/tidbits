40b7bc76c8add88c0673fdc739266cb7fc5ea1ab
Ewan Mellor <ewan@tipbit.com>
2013-06-13 12:35:56 -0700

    Added TBPCH.h, which imports all the things that would be reasonable to have in the default namespace all the time (i.e. go in a .pch).

---
+++ Tidbits/TBPCH.h
@@ -0,0 +1,30 @@
+//
+//  TBPCH.h
+//  Tidbits
+//
+//  Created by Ewan Mellor on 6/13/13.
+//  Copyright (c) 2013 Tipbit, Inc. All rights reserved.
+//
+
+#ifndef Tidbits_TBPCH_h
+#define Tidbits_TBPCH_h
+
+#import "Dispatch.h"
+#import "LoggingMacros.h"
+#import "NSArray+Map.h"
+#import "NSData+Base64.h"
+#import "NSDate+ISO8601.h"
+#import "NSDictionary+Map.h"
+#import "NSDictionary+Misc.h"
+#import "NSMutableData+AppendByte.h"
+#import "NSMutableData+UTF8.h"
+#import "NSMutableDictionary+Misc.h"
+#import "NSString+IsNotWhitespace.h"
+#import "NSString+Misc.h"
+#import "NSString+URL.h"
+#import "NSUserDefaults+Default.h"
+#import "StandardBlocks.h"
+#import "SynthesizeAssociatedObject.h"
+#import "TBAsserts.h"
+
+#endif
