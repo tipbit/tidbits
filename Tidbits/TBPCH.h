dd13e3553ad85081be207b26faa3ae3790694a7b
Ewan Mellor <ewan@tipbit.com>
2013-06-16 19:41:07 -0700

    Added NSDate+Ext to CopyFiles and TBPCH.

--- Tidbits/TBPCH.h
+++ Tidbits/TBPCH.h
@@ -14,6 +14,7 @@
 #import "NSArray+Map.h"
 #import "NSData+Base64.h"
 #import "NSData+NSInputStream.h"
+#import "NSDate+Ext.h"
 #import "NSDate+ISO8601.h"
 #import "NSDictionary+Map.h"
 #import "NSDictionary+Misc.h"
