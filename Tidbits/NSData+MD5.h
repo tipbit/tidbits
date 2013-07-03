8b8398f44108328dd95bb18ce38b4492ed55a4dd
Ewan Mellor <ewan@tipbit.com>
2013-07-03 14:21:46 -0700

    Added NSData+MD5.

---
+++ Tidbits/NSData+MD5.h
@@ -0,0 +1,15 @@
+//
+//  NSData+MD5.h
+//  Tidbits
+//
+//  Created by Ewan Mellor on 7/3/13.
+//  Copyright (c) 2013 Tipbit, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (MD5)
+
+-(NSString*)md5;
+
+@end
