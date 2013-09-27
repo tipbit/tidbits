24975439040fd4a990a3237b6ef5bd25de15741b
Nat Ballou <natba@tipbit.com>
2013-09-27 15:48:38 -0700

    Tidy up per user settings header

--- Tidbits/NSUserDefaults+PerUser.h
+++ Tidbits/NSUserDefaults+PerUser.h
@@ -10,10 +10,15 @@
 
 @interface NSUserDefaults (PerUser)
 
+// Per user keys
 +(NSString*)PUKey:(NSString*) key;
 +(NSString*)PUKey:(NSString*) key user:(NSString*) TBUser;
+
+// Per user Bool settings
 +(BOOL)getPUBool:(NSString*)key;
 +(void)setPUBool:(BOOL)value forKey:(NSString*)key;
+
+// Per user string settings
 +(NSString*)getPUString:(NSString*)key;
 +(void)setPUString:(NSString*)value forKey:(NSString*)key;
 
