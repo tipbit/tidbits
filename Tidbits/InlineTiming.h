4a03ad4920d984d0cd5334b7d65b5e7dad08eb34
Ewan Mellor <ewan@tipbit.com>
2013-10-23 16:59:06 -0700

    Make the array size a #define.

--- Tidbits/InlineTiming.h
+++ Tidbits/InlineTiming.h
@@ -10,15 +10,17 @@
 
 #if DEBUG
 
+#define INLINE_TIMING_MAX 32
+
 #define InlineTimingMark                                                                 \
-    assert(INLINE_TIMING_INDEX < 32);                                                    \
+    assert(INLINE_TIMING_INDEX < INLINE_TIMING_MAX);                                     \
     INLINE_TIMING_TIMES[INLINE_TIMING_INDEX] = [NSDate timeIntervalSinceReferenceDate];  \
     INLINE_TIMING_LINES[INLINE_TIMING_INDEX] = __LINE__;                                 \
     INLINE_TIMING_INDEX++
 
 #define InlineTimingStart                                                                \
-    NSTimeInterval INLINE_TIMING_TIMES[32];                                              \
-    NSUInteger INLINE_TIMING_LINES[32];                                                  \
+    NSTimeInterval INLINE_TIMING_TIMES[INLINE_TIMING_MAX];                               \
+    NSUInteger INLINE_TIMING_LINES[INLINE_TIMING_MAX];                                   \
     NSUInteger INLINE_TIMING_INDEX = 0;                                                  \
     InlineTimingMark
 
