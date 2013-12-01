83c713e2cc5ac30a40feb9bfdbff223d7f350aa2
Ewan Mellor <ewan@tipbit.com>
2013-12-01 12:17:20 -0800

    Add support for XcodeColors, highlighting the slowest delta in red.
    See https://github.com/robbiehanson/XcodeColors for XcodeColors.

--- Tidbits/InlineTiming.m
+++ Tidbits/InlineTiming.m
@@ -13,7 +13,23 @@
 
 #if INLINE_TIMING_ENABLED
 
+#define ESCAPE @"\033["
+#define FG_RED ESCAPE @"fg200,0,0;"
+#define FG_OFF ESCAPE @"fg;"
+
+static NSString* fg_red;
+static NSString* fg_off;
+
+
 @implementation InlineTiming
+
+
++(void)initialize {
+    char *xcode_colors = getenv("XcodeColors");
+    bool enabled = xcode_colors && 0 == strcmp(xcode_colors, "YES");
+    fg_red = enabled ? FG_RED : @"";
+    fg_off = enabled ? FG_OFF : @"";
+}
 
 
 -(id)init:(NSUInteger)line {
@@ -48,22 +64,38 @@
     if (total_time < budget)
         return;
 
+    if (count == 2) {
+        NSLog(@"Timing for %s:%u: %lf", func, line, times[1] - times[0]);
+        return;
+    }
+
+    NSTimeInterval deltas[INLINE_TIMING_MAX];
     NSTimeInterval max_delta = 0;
     NSUInteger max_line = 0;
-    NSMutableString* str = [NSMutableString string];
     NSTimeInterval previous = times[0];
     for (NSUInteger i = 1; i < count; i++) {
         NSTimeInterval time = times[i];
         NSTimeInterval delta = time - previous;
-        [str appendFormat:@" %lf", delta];
+        deltas[i] = delta;
         previous = time;
         if (delta > max_delta) {
             max_delta = delta;
             max_line = lines[i];
         }
     }
-    NSLog(@"Timings for %s:%u:%@ = %lf", func, line, str, total_time);
-    NSLog(@"Timings for %s:%u: max delta was %lf(%0.2lf%%) at line %u", func, line, max_delta, 100 * max_delta / total_time, max_line);
+
+    NSMutableString* str = [NSMutableString string];
+    for (NSUInteger i = 1; i < count; i++) {
+        NSTimeInterval delta = deltas[i];
+        bool is_max = max_delta - delta < 0.001;
+        if (is_max)
+            [str appendString:fg_red];
+        [str appendFormat:@" %lf", delta];
+        if (is_max)
+            [str appendString:fg_off];
+    }
+
+    NSLog(@"Timings for %s:%u:%@ = %lf; max of %lf(%0.2lf%%) at line %u", func, line, str, total_time, max_delta, 100 * max_delta / total_time, max_line);
 }
 
 @end
