23543c857fba7b4297e9c890e11671febbc6e3b9
Ewan Mellor <ewan@tipbit.com>
2013-12-15 01:31:48 -0800

    Switch to appledoc-style comments.

--- Tidbits/NSArray+Map.h
+++ Tidbits/NSArray+Map.h
@@ -12,25 +12,25 @@
 
 @interface NSArray (Map)
 
-/*!
- * @abstract Create a new NSArray with the contents set to x for each x in self
+/**
+ * Create a new NSArray with the contents set to x for each x in self
  * where filter(x) returns true.
  */
 -(NSArray*)filter:(predicate_t)filter;
 
-/*!
- * @abstract Create a new NSArray with the contents set to mapper(x) for each x in self.
+/**
+ * Create a new NSArray with the contents set to mapper(x) for each x in self.
  *
- * @discussion mapper may return nil, in which case no entry is added to the result
+ * mapper may return nil, in which case no entry is added to the result
  * (i.e. the result will be shorter than self).
  */
 -(NSArray*) map:(id_to_id_t)mapper;
 
-/*!
- * @abstract Create a new NSMutableArray with the contents determined by calls to mapper
+/**
+ * Create a new NSMutableArray with the contents determined by calls to mapper
  * for each x in self.
  *
- * @discussion This is designed for a continuation-passing style where each call to
+ * This is designed for a continuation-passing style where each call to
  * mapper is serialised, and they are chained from one to the next.  mapper(x, callback)
  * is called for each x in self, and mapper should call callback(new_x) with the value
  * to be added to the result of this call.  mapper may call callback(nil), in which case
@@ -45,9 +45,9 @@
 -(void) map_async:(id_to_id_async_t)mapper onSuccess:(NSMutableArrayBlock)onSuccess;
 
 /**
- * @abstract Create a new NSMutableDictionary with the contents set to k -> mapper(k) for each k in self.
+ * Create a new NSMutableDictionary with the contents set to k -> mapper(k) for each k in self.
  *
- * @discussion mapper may return nil, in which case no entry is added to the result
+ * mapper may return nil, in which case no entry is added to the result
  * (i.e. the result will be smaller than self).
  *
  * Another way to think about this is as [NSMutableDictionary dictionaryWithObjects:[self map:mapper] forKeys:self],
@@ -56,18 +56,18 @@
 -(NSMutableDictionary*)dictionaryWithKeysAndMappedValues:(id_to_id_t)mapper;
 
 /**
- * @abstract Create a new NSMutableDictionary with the contents set to k -> @[v1, ..., vn], where k = mapper(v) for each v in self.
+ * Create a new NSMutableDictionary with the contents set to k -> @[v1, ..., vn], where k = mapper(v) for each v in self.
  * @[v1, ..., vn] here denotes an NSMutableArray with all values in self that are mapped to that particular key.
  *
- * @discussion mapper may return nil, in which case no entry is added to the result
+ * mapper may return nil, in which case no entry is added to the result
  * (i.e. the result will be smaller than self).
  */
 -(NSMutableDictionary *)dictionaryWithValuesAndMappedKeys:(id_to_id_t)mapper;
 
 /**
- * @abstract Create a new NSMutableDictionary with the contents set to k -> v, where k = mapper(v) for each v in self.
+ * Create a new NSMutableDictionary with the contents set to k -> v, where k = mapper(v) for each v in self.
  *
- * @discussion mapper may return nil, in which case no entry is added to the result
+ * mapper may return nil, in which case no entry is added to the result
  * (i.e. the result will be smaller than self).
  *
  * This call is intended for the case where mapper returns a unique key for each member of self.
