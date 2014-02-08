5d32c540fd1857bec7d5a4e35be357150e0a0ecb
Ewan Mellor <ewan@tipbit.com>
2014-02-08 01:20:59 -0800

    Added tests for dictAtIndex, objectAtIndex:withDefault:, and arrayWithEnumeration.

--- TidbitsTests/NSArray+MiscTests.m
+++ TidbitsTests/NSArray+MiscTests.m
@@ -69,4 +69,40 @@
 }
 
 
+-(void)testDictAtIndex {
+    NSDictionary* dict = @{@1: @2};
+    NSArray* input = @[@0, dict, @1];
+
+    XCTAssertEqualObjects([input dictAtIndex:0], nil);
+    XCTAssertEqualObjects([input dictAtIndex:1], dict);
+    XCTAssertEqualObjects([input dictAtIndex:2], nil);
+    XCTAssertEqualObjects([input dictAtIndex:3], nil);
+}
+
+
+-(void)testObjectAtIndexWithDefault {
+    NSDictionary* dict = @{@1: @2};
+    NSArray* input = @[@0, dict, @1];
+
+    XCTAssertEqualObjects([input objectAtIndex:0 withDefault:@6], @0);
+    XCTAssertEqualObjects([input objectAtIndex:1 withDefault:@42], dict);
+    XCTAssertEqualObjects([input objectAtIndex:2 withDefault:nil], @1);
+    XCTAssertEqualObjects([input objectAtIndex:3 withDefault:@7], @7);
+    XCTAssertEqualObjects([input objectAtIndex:4 withDefault:nil], nil);
+    XCTAssertEqualObjects([input objectAtIndex:NSNotFound withDefault:@7], @7);
+}
+
+
+-(void)testArrayWithEnumerationFromArray {
+    NSArray* input = @[@0, @1];
+    XCTAssertEqualObjects([NSArray arrayWithEnumeration:input], input);
+}
+
+
+-(void)testArrayWithEnumerationEmpty {
+    NSArray* input = @[];
+    XCTAssertEqualObjects([NSArray arrayWithEnumeration:input], input);
+}
+
+
 @end
