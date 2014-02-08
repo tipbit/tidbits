ca692d13c454c79c8a7e44c2ed7224baac4f1915
Ewan Mellor <ewan@tipbit.com>
2014-02-07 23:35:47 -0800

    Added tests for NSUUID+Misc.

---
+++ TidbitsTests/NSUUID+MiscTests.m
@@ -0,0 +1,38 @@
+//
+//  NSUUID+MiscTests.m
+//  Tidbits
+//
+//  Created by Ewan Mellor on 2/7/14.
+//  Copyright (c) 2014 Tipbit, Inc. All rights reserved.
+//
+
+#import "TBTestCaseBase.h"
+
+#import "NSUUID+Misc.h"
+
+
+@interface NSUUID_MiscTests : TBTestCaseBase
+
+@end
+
+
+@implementation NSUUID_MiscTests
+
+
+-(void)testUUIDStringBase64url {
+    NSUUID* input = [[NSUUID alloc] initWithUUIDString:@"5F62567A-74C2-4D32-B649-D26D762433BF"];
+    NSString* expected = @"X2JWenTCTTK2SdJtdiQzvw";
+    NSString* result = [input UUIDStringBase64url];
+    XCTAssertEqualObjects(result, expected);
+}
+
+
+-(void)testUUIDStringBase64urlDashUnderscore {
+    NSUUID* input = [[NSUUID alloc] initWithUUIDString:@"2e1336fd-4349-485f-8bad-df45b92ae32e"];
+    NSString* expected = @"LhM2_UNJSF-Lrd9FuSrjLg";
+    NSString* result = [input UUIDStringBase64url];
+    XCTAssertEqualObjects(result, expected);
+}
+
+
+@end
