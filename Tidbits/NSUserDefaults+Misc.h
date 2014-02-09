2b9af3d1d341e0aeec95414a1a7d5565f49c056e
Ewan Mellor <ewan@tipbit.com>
2014-02-08 21:42:06 -0800

    Add tb_reloadFromMainBundle.
    This loads the plist that contains the main settings, and uses
    registerDefaults to put those settings into [NSUserDefaults standardUserDefaults].
    
    This can be used to load settings that didn't get loaded initially because
    the screen was locked.

--- Tidbits/NSUserDefaults+Misc.h
+++ Tidbits/NSUserDefaults+Misc.h
@@ -10,6 +10,18 @@
 
 @interface NSUserDefaults (Misc)
 
+/**
+ * Load the plist that contains the main settings, and use registerDefaults to put those settings into [NSUserDefaults standardUserDefaults].
+ *
+ * This can be used to load settings that didn't get loaded initially because the screen was locked.
+ */
++(void)tb_reloadFromMainBundle;
+
+/**
+ * A wrapper to standardUserDefaults, except it won't load the settings if [UIApplication isProtectedDataAvailable] is false.
+ *
+ * This means that we won't load blank settings when the screen is locked.
+ */
 +(NSUserDefaults *)tb_standardUserDefaults;
 
 /**
--- Tidbits/NSUserDefaults+Misc.m
+++ Tidbits/NSUserDefaults+Misc.m
@@ -16,6 +16,34 @@
 @implementation NSUserDefaults (Misc)
 
 
++(void)tb_reloadFromMainBundle {
+    NSString* plistName = [NSString stringWithFormat:@"%@.plist", [[NSBundle mainBundle] bundleIdentifier]];
+
+    NSArray *libraryDirs = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
+    if (libraryDirs.count == 0) {
+        NSLog(@"Failed to find Library directory!  This is a very ill phone!");
+        return;
+    }
+    NSString *libraryDir = libraryDirs[0];
+    NSString* preferencesDir = [libraryDir stringByAppendingPathComponent:@"Preferences"];
+    NSString* defaultsPath = [preferencesDir stringByAppendingPathComponent:plistName];
+    NSDictionary* plistContents = [NSDictionary dictionaryWithContentsOfFile:defaultsPath];
+
+    // Don't log the settings in production builds, for privacy reasons.
+    NSString* loggablePlistContents =
+#if DEBUG
+    [plistContents description];
+#else
+    @"<redacted>";
+#endif
+    NSLog(@"Reloading %@ as defaults from %@", loggablePlistContents, defaultsPath);
+
+    NSUserDefaults* defaults = [NSUserDefaults tb_standardUserDefaults];
+    [defaults registerDefaults:plistContents];
+    [defaults tb_synchronize];
+}
+
+
 static bool we_have_initialized = false;
 +(NSUserDefaults *)tb_standardUserDefaults {
     UIApplication* app = [UIApplication sharedApplication];  // app is nil if we're running unit tests.
