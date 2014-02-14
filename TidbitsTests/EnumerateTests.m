4d93db25276eddf8832a6f0b94ca3ad667bb54c6
Ewan Mellor <ewan@tipbit.com>
2014-02-14 12:44:27 -0800

    More tests.

--- TidbitsTests/EnumerateTests.m
+++ TidbitsTests/EnumerateTests.m
@@ -33,4 +33,32 @@
 }
 
 
+-(void)testEnumerateDifferentLengths {
+    NSArray* a1 = @[@1, @2];
+    NSArray* a2 = @[@4, @5, @6];
+    NSMutableArray* result = [NSMutableArray array];
+    NSArray* expected = @[@5, @7];
+
+    [Enumerate pairwiseOver:a1 and:a2 usingBlock:^(id obj1, id obj2) {
+        [result addObject:@([obj1 intValue] + [obj2 intValue])];
+    }];
+
+    XCTAssertEqualObjects(result, expected);
+}
+
+
+-(void)testEnumerateNil {
+    NSArray* a1 = nil;
+    NSArray* a2 = @[@4, @5, @6];
+    NSMutableArray* result = [NSMutableArray array];
+    NSArray* expected = @[];
+
+    [Enumerate pairwiseOver:a1 and:a2 usingBlock:^(id obj1, id obj2) {
+        [result addObject:@([obj1 intValue] + [obj2 intValue])];
+    }];
+
+    XCTAssertEqualObjects(result, expected);
+}
+
+
 @end
