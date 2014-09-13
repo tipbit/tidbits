3ff8d1d6b11ff7bf00ebc6ecc454d3dcce91292f
Ewan Mellor <ewan@tipbit.com>
2014-09-12 21:53:03 -0700

    Define NSFoundationVersionNumber_iOS_7_1 if it's not already defined, so that we can compile this code under Xcode 5 / iOS 7.1.

--- Tidbits/FeatureMacros.h
+++ Tidbits/FeatureMacros.h
@@ -9,15 +9,15 @@
 #ifndef Tidbits_FeatureMacros_h
 #define Tidbits_FeatureMacros_h
 
+#ifndef NSFoundationVersionNumber_iOS_7_1
+#define NSFoundationVersionNumber_iOS_7_1 1047.25
+#endif
+
+
 #define IS_IPAD (UIDevice.currentDevice.userInterfaceIdiom == UIUserInterfaceIdiomPad)
 #define IS_IPHONE5 ((UIScreen.mainScreen.bounds.size.height > 567) || (UIScreen.mainScreen.bounds.size.width > 567))
 #define IS_IOS6 (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_6_1)
-#ifdef NSFoundationVersionNumber_iOS_7_1
 #define IS_IOS7 (floor(NSFoundationVersionNumber) >= NSFoundationVersionNumber_iOS_6_1 && floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_7_1)
-#else
-#define IS_IOS7 (floor(NSFoundationVersionNumber) >  NSFoundationVersionNumber_iOS_6_1)
-#endif
-
 #define IS_IOS8 (floor(NSFoundationVersionNumber) >  NSFoundationVersionNumber_iOS_7_1)
 
 
