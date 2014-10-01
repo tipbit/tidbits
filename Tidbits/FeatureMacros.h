264e533ff274527e00f5059d7a4f3cc88d206bd6
Kurt Bosselman <kurtbosselman@q.com>
2014-10-01 15:55:26 -0700

    add IS_IPHONE4

--- Tidbits/FeatureMacros.h
+++ Tidbits/FeatureMacros.h
@@ -15,6 +15,7 @@
 
 
 #define IS_IPAD (UIDevice.currentDevice.userInterfaceIdiom == UIUserInterfaceIdiomPad)
+#define IS_IPHONE4 ((UIScreen.mainScreen.bounds.size.height == 480) || (UIScreen.mainScreen.bounds.size.width == 480))
 #define IS_IPHONE5 ((UIScreen.mainScreen.bounds.size.height == 568) || (UIScreen.mainScreen.bounds.size.width == 568))
 #define IS_IPHONE6 ((UIScreen.mainScreen.bounds.size.height == 667) || (UIScreen.mainScreen.bounds.size.width == 667))
 #define IS_IPHONE6PLUS ((UIScreen.mainScreen.bounds.size.height == 414) || (UIScreen.mainScreen.bounds.size.width == 414))
