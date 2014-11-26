73ad697031c7aeed8d9175f24e7f31498ccde811
Ewan Mellor <ewan@tipbit.com>
2014-11-25 21:23:33 -0800

    Move the definition of the NSLog*, DLog, and ELog macros to a separate file so that we can redefine them on a per-file basis to include a prefix on each message.
    This also changes NSLogUser to be async rather than sync (via a
    change to use LOG_ASYNC_INFO).

--- Tidbits/LoggingMacros.h
+++ Tidbits/LoggingMacros.h
@@ -65,32 +65,18 @@
 #define LOG_LEVEL_INFO    (LOG_FLAG_INFO   | LOG_LEVEL_USER  )   // 0...011111
 #define LOG_LEVEL_DEBUG   (LOG_FLAG_DEBUG  | LOG_LEVEL_INFO  )   // 0...111111
 
-#endif
-
-#define NSLogError(__fmt, ...) LOG_C_MAYBE(LOG_ASYNC_ERROR, LOG_LEVEL_ERROR, LOG_FLAG_ERROR, 0, __fmt, ##__VA_ARGS__)
-#define NSLogWarn(__fmt, ...) LOG_C_MAYBE(LOG_ASYNC_WARN, LOG_LEVEL_WARN, LOG_FLAG_WARN, 0, __fmt, ##__VA_ARGS__)
-#define NSLogInfo(__fmt, ...) LOG_C_MAYBE(LOG_ASYNC_INFO, LOG_LEVEL_INFO, LOG_FLAG_INFO, 0, __fmt, ##__VA_ARGS__)
-
-//our logging already prints out the function and line.
-//#define NSLogUser(MSG, ...) NSLog((@"[U] %s:%d "MSG), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
-//#define NSLogUser(MSG, ...) NSLog((@"[U] "MSG), ##__VA_ARGS__)
-#define NSLogUser(__fmt, ...) LOG_C_MAYBE(LOG_ASYNC_ERROR, LOG_LEVEL_USER, LOG_FLAG_USER, 0, __fmt, ##__VA_ARGS__)
-
-
-#define NSLog(__fmt, ...) LOG_C_MAYBE(LOG_ASYNC_INFO,    LOG_LEVEL_INFO, LOG_FLAG_INFO,    0, __fmt, ##__VA_ARGS__)
-
-#if DEBUG
-#define DLog(__fmt, ...) LOG_C_MAYBE(LOG_ASYNC_DEBUG, LOG_LEVEL_DEBUG, LOG_FLAG_DEBUG, 0, __fmt, ##__VA_ARGS__)
-#else
-#define DLog(...)
-#endif
-
-#define ELog(__err) {if (__err) NSLogError(@"%@", __err)}
-
 #if DEBUG
 #define CALLSTACK [NSThread callStackSymbols]
 #define CALLEDBY [NSThread callingFunction]
 #else
 #define CALLSTACK @""
 #define CALLEDBY @""
+#endif
+
+
+#define LoggingMacrosTag(__s) @"⟦" __s @"⟧"
+
+
+#include "LoggingMacrosWrappers.h"
+
 #endif
---
+++ Tidbits/LoggingMacrosWrappers.h
@@ -0,0 +1,52 @@
+//
+//  LoggingMacrosWrappers.h
+//  Tidbits
+//
+//  Created by Ewan Mellor on 11/25/14.
+//  Copyright (c) 2014 Tipbit, Inc. All rights reserved.
+//
+
+/*
+ You may include this file at any point, in order to redefine all the NSLog*, DLog, and ELog macros.
+ In particular, you can do this to set a prefix for each log message, like this:
+
+ #define LoggingMacrosPrefix @"SomePrefix"
+ #include "LoggingMacrosWrappers.h"
+
+ You can make the prefix stand out by wrapping it like this:
+
+ #define LoggingMacrosPrefix LoggingMacrosTag(@"MyTag")
+ #include "LoggingMacrosWrappers.h"
+
+ This will give you a prefix like this: ⟦MyTag⟧
+
+ */
+
+#undef NSLog
+#undef NSLogError
+#undef NSLogWarn
+#undef NSLogInfo
+#undef NSLogUser
+#undef DLog
+#undef ELog
+
+#undef _LoggingMacrosPrefix
+#ifdef LoggingMacrosPrefix
+#define _LoggingMacrosPrefix LoggingMacrosPrefix @" "
+#else
+#define _LoggingMacrosPrefix
+#endif
+
+#define NSLogError(__fmt, ...) LOG_C_MAYBE(LOG_ASYNC_ERROR, LOG_LEVEL_ERROR, LOG_FLAG_ERROR, 0, _LoggingMacrosPrefix __fmt, ##__VA_ARGS__)
+#define NSLogWarn(__fmt, ...)  LOG_C_MAYBE(LOG_ASYNC_WARN,  LOG_LEVEL_WARN,  LOG_FLAG_WARN,  0, _LoggingMacrosPrefix __fmt, ##__VA_ARGS__)
+#define NSLogInfo(__fmt, ...)  LOG_C_MAYBE(LOG_ASYNC_INFO,  LOG_LEVEL_INFO,  LOG_FLAG_INFO,  0, _LoggingMacrosPrefix __fmt, ##__VA_ARGS__)
+#define NSLogUser(__fmt, ...)  LOG_C_MAYBE(LOG_ASYNC_INFO,  LOG_LEVEL_USER,  LOG_FLAG_USER,  0, _LoggingMacrosPrefix __fmt, ##__VA_ARGS__)
+#define NSLog(__fmt, ...)      LOG_C_MAYBE(LOG_ASYNC_INFO,  LOG_LEVEL_INFO,  LOG_FLAG_INFO,  0, _LoggingMacrosPrefix __fmt, ##__VA_ARGS__)
+
+#if DEBUG
+#define DLog(__fmt, ...) LOG_C_MAYBE(LOG_ASYNC_DEBUG, LOG_LEVEL_DEBUG, LOG_FLAG_DEBUG, 0, _LoggingMacrosPrefix __fmt, ##__VA_ARGS__)
+#else
+#define DLog(...)
+#endif
+
+#define ELog(__err) {if (__err) NSLogError(_LoggingMacrosPrefix @"%@", __err)}
