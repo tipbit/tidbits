e366eb03881df3924b785636a1d248f381dc0148
Ewan Mellor <ewan@tipbit.com>
2015-05-23 19:14:22 -0700

    Add some doccomments.

--- Tidbits/UTI.h
+++ Tidbits/UTI.h
@@ -14,7 +14,13 @@
  */
 extern BOOL utiConformsTo(NSString * child, NSString * parent);
 
-extern NSString* utiFilenameToMIME(NSString* fname);
+/**
+ * @return The MIME type corresponding to the given filename (its extension, specifically).
+ * "message/rfc822" for .eml files (even though that's not in the UTI database).
+ * "application/octet-stream" if there's no match in the UTI database (so this function will
+ * never return nil).
+ */
+extern NSString * utiFilenameToMIME(NSString* fname);
 
 /**
  * @return The UTI type corresponding to the given filename (its extension, specifically) or nil if
@@ -22,9 +28,23 @@ extern NSString* utiFilenameToMIME(NSString* fname);
  */
 extern NSString * utiFilenameToType(NSString * filename);
 
-extern NSString* utiMIMEToExtension(NSString* mime);
-extern NSString * utiToMIME(NSString * uti);
+/**
+ * @return The human-readable description corresponding to the given UTI
+ * or nil if there's no match in the UTI database.
+ */
 extern NSString * utiHumanReadableDescription(NSString * uti);
+
+/**
+ * @return YES if the given UTI is an image (i.e. it conforms to public.image).
+ */
+extern BOOL utiIsImage(NSString * uti);
+
+/**
+ * @return The file extension corresponding to the given MIME type.
+ * "eml" for message/rfc822 (even though that's not in the UTI database).
+ * nil if there's no match in the UTI database.
+ */
+extern NSString * utiMIMEToExtension(NSString* mime);
 
 /**
  * @return The UTI type corresponding to the given MIME type or nil if there's no match in the UTI database.
@@ -32,9 +52,9 @@ extern NSString * utiHumanReadableDescription(NSString * uti);
 extern NSString * utiMIMEToType(NSString * mime);
 
 /**
- * @return YES if the given UTI is an image (i.e. it conforms to public.image).
+ * @return The MIME type corresponding to the given UTI type or nil if there's no match in the UTI database.
  */
-extern BOOL utiIsImage(NSString * uti);
+extern NSString * utiToMIME(NSString * uti);
 
 /**
  * Detect the type of the given data, using magic numbers or whatever else the file format supports.
