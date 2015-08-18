59c13cec4f6e56b9a03aba6a49be6bd52f052d97
Ewan Mellor <ewan@tipbit.com>
2015-08-18 00:06:27 -0700

    Round the time to the nearest msec when logging, rather than truncating.
    This matches the change made in c555d4d0a9.

--- Tidbits/LogFormatter.m
+++ Tidbits/LogFormatter.m
@@ -77,7 +77,7 @@
     struct tm tm;
     NSTimeInterval ts = [logMessage->timestamp timeIntervalSince1970];
     time_t ts_whole = (time_t)ts;
-    int ts_frac = (int)((ts - (double)ts_whole) * 1000.0);
+    int ts_frac = (int)round(((ts - (double)ts_whole) * 1000.0));
     gmtime_r(&ts_whole, &tm);
     // Using snprintf for the fixed-length fields is 26-29% faster than putting it all in the stringWithFormat call.
     snprintf(time_level_str, 30, "%4d-%02d-%02d %02d:%02d:%02d.%03d %5s", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, ts_frac, logLevelToStr(logMessage->logLevel));
@@ -116,7 +116,7 @@ static const char * logLevelToStr(int level) {
     struct tm tm;
     NSTimeInterval ts = [logMessage->timestamp timeIntervalSince1970];
     time_t ts_whole = (time_t)ts;
-    int ts_frac = (int)((ts - (double)ts_whole) * 1000.0);
+    int ts_frac = (int)round((ts - (double)ts_whole) * 1000.0);
     gmtime_r(&ts_whole, &tm);
     snprintf(time_level_str, sizeof(time_level_str), "%4d-%02d-%02d %02d:%02d:%02d.%03d %5s", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, ts_frac, logLevelToStr(logMessage->logLevel));
     return [NSString stringWithFormat:@"%s %s:%i | %@", time_level_str, logMessage->function, logMessage->lineNumber, logMessage->logMsg];
@@ -134,7 +134,7 @@ static const char * logLevelToStr(int level) {
     struct tm tm;
     NSTimeInterval ts = [logMessage->timestamp timeIntervalSince1970];
     time_t ts_whole = (time_t)ts;
-    int ts_frac = (int)((ts - (double)ts_whole) * 1000.0);
+    int ts_frac = (int)round((ts - (double)ts_whole) * 1000.0);
     localtime_r(&ts_whole, &tm);
     // Using snprintf for the fixed-length fields is 26-29% faster than putting it all in the stringWithFormat call.
     snprintf(time_level_str, sizeof(time_level_str), "%c %02d:%02d:%02d.%03d", logLevelToChar(logMessage->logLevel), tm.tm_hour, tm.tm_min, tm.tm_sec, ts_frac);
@@ -186,7 +186,7 @@ static char logLevelToChar(int level) {
     struct tm tm;
     NSTimeInterval ts = [logMessage->timestamp timeIntervalSince1970];
     time_t ts_whole = (time_t)ts;
-    int ts_frac = (int)((ts - (double)ts_whole) * 1000.0);
+    int ts_frac = (int)round((ts - (double)ts_whole) * 1000.0);
     localtime_r(&ts_whole, &tm);
     // Using snprintf for the fixed-length fields is 26-29% faster than putting it all in the stringWithFormat call.
     snprintf(time_level_str, sizeof(time_level_str), "%c %02d:%02d:%02d.%03d", logLevelToChar(logMessage->logLevel), tm.tm_hour, tm.tm_min, tm.tm_sec, ts_frac);
